d1e8891c47423db91a86e1478df8adc0e622cff0
Serhii Dulin <64526933+SergDulin@users.noreply.github.com>
2023-07-03 23:46:24 +0300

    нижньому регістрі, а значеннями - їх номери телефонів. 2.	Додаємо вміст функцій handler згідно їх функціоналу: -	add_contact(name, phone) - ця функція додає новий контакт до словника contacts. Вона приймає два аргументи - name (ім'я контакту) і phone (номер телефону контакту). Спочатку ім'я перетворюється в нижній регістр, а потім контакт додається до словника contacts. Якщо контакт вже існує, його номер телефону оновлюється. Функція повертає повідомлення про успішне додавання контакту або оновлення номеру телефону. -	change_phone(name, phone) - ця функція змінює номер телефону існуючого контакту. Вона приймає два аргументи - name (ім'я контакту) і phone (новий номер телефону). Функція перевіряє, чи існує контакт з таким ім'ям у словнику contacts. Якщо такий контакт знайдено, номер його телефону оновлюється на новий. Якщо контакт не знайдено, повертається повідомлення про невдалу спробу зміни номеру телефону. -	get_phone(name) - ця функція повертає номер телефону контакту за його іменем. Вона приймає один аргумент - name (ім'я контакту). Функція шукає контакти, чиє ім'я містить задане значення (незалежно від регістру). Якщо знайдено контакти, повертається повідомлення з номерами телефонів цих контактів. Якщо контакти не знайдено, повертається повідомлення про відсутність контактів з таким іменем. -	show_all_contacts() - ця функція виводить на екран усі контакти зі словника contacts. Якщо словник не містить жодного контакту, повертається повідомлення про їх відсутність.

--- goit_hw-09.py
+++ goit_hw-09.py
@@ -14,16 +14,32 @@ contacts = {}
 
 @input_error
 def add_contact(name, phone):
-    
+    contacts[name.lower()] = phone
+    return "Contact added successfully."
 
 @input_error
 def change_phone(name, phone):
-    
+    matching_contacts = [contact_name for contact_name in contacts.keys() if contact_name.lower() == name.lower()]
+    if matching_contacts:
+        contacts[matching_contacts[0]] = phone
+        return "Phone number updated successfully."
+    else:
+        return "Contact not found."
+
 @input_error
 def get_phone(name):
-   
+    matching_contacts = [f"The phone number for {contact_name.title()} is {phone}" for contact_name, phone in contacts.items() if name.lower() in contact_name.lower()]
+    if matching_contacts:
+        return "\n".join(matching_contacts)
+    else:
+        return "No contacts found with that name."
 
 def show_all_contacts():
+    if contacts:
+        all_contacts = "\n".join([f"{name.title()}: {phone}" for name, phone in contacts.items()])
+        return all_contacts
+    else:
+        return "No contacts found."
     
 
 def main():
