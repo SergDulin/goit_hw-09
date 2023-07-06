c634b51104fe0453360339fe2ca9ccea8e135a13
Serhii Dulin <64526933+SergDulin@users.noreply.github.com>
2023-07-06 23:05:35 +0300

    1. Функція `add_contact` отримала зміну в сигнатурі, тепер вона приймає аргументи `*args`, що дозволяє передавати будь-яку кількість аргументів безпосередньо в функцію. 2. У функції `change_phone` додана перевірка на наявність контакту з вказаним іменем `name` в словнику `contacts`. Якщо контакт не знайдено, викидається виключення `KeyError`. 3. Функція `get_phone` не зазнала змін. 4. Функція `show_all_contacts` була збережена, проте змінена її реалізація. Тепер вона перевіряє наявність контактів у словнику `contacts` та повертає відповідну повідомлення. 5. Додані дві нові функції `hello_command` та `goodbye_command` для вітання і прощання відповідно. 6. Створена функція `komand`, яка приймає розбиту на частини команду `command_parts` і виконує відповідні дії залежно від команди. 7. Створений словник `command_handlers`, який містить зв'язки між командами та відповідними функціями-обробниками. 8. Головна функція `main` була змінена. Тепер вона викликає функцію `komand` для обробки команди і виводить результат. Якщо результат є повідомленням про прощання (`"Goodbye!"`), цикл `while` припиняється, і програма завершується. 9. Змінений спосіб запуску головної функції `main` для використання нової логіки.
    Ці зміни дозволяють зробити код більш компактним і керованим, використовуючи словник `command_handlers` для визначення обробників команд та функцію `komand` для знаходження відповідного обробника на основі отриманої команди.

--- goit_hw-09.py
+++ goit_hw-09.py
@@ -5,7 +5,7 @@ def input_error(func):
         except KeyError:
             return "Contact not found."
         except ValueError:
-            return "Invalid input. Please provide both name and phone number."
+            return "Invalid input. Please provide valid arguments."
         except IndexError:
             return "Invalid input. Please provide both name and phone number."
     return inner
@@ -13,70 +13,80 @@ def input_error(func):
 contacts = {}
 
 @input_error
-def add_contact(name, phone):
-    contacts[name.lower()] = phone
+def add_contact(*args):
+    if len(args) < 2:
+        raise IndexError
+    name = " ".join(args[:-1]).lower()
+    phone = args[-1]
+    contacts[name] = phone
     return "Contact added successfully."
 
 @input_error
-def change_phone(name, phone):
-    matching_contacts = [contact_name for contact_name in contacts.keys() if contact_name.lower() == name.lower()]
-    if matching_contacts:
-        contacts[matching_contacts[0]] = phone
-        return "Phone number updated successfully."
-    else:
-        return "Contact not found."
+def change_phone(*args):
+    if len(args) < 2:
+        raise IndexError
+    name = " ".join(args[:-1]).lower()
+    phone = args[-1]
+    if name not in contacts:
+        raise KeyError
+    contacts[name] = phone
+    return "Phone number updated successfully."
 
 @input_error
 def get_phone(name):
-    matching_contacts = [f"The phone number for {contact_name.title()} is {phone}" for contact_name, phone in contacts.items() if name.lower() in contact_name.lower()]
+    matching_contacts = [f"{contact_name.title()} is {phone}" for contact_name, phone in contacts.items() if name.lower() in contact_name.lower()]
     if matching_contacts:
         return "\n".join(matching_contacts)
     else:
         return "No contacts found with that name."
 
+@input_error
 def show_all_contacts():
     if contacts:
-        all_contacts = "\n".join([f"{name.title()}: {phone}" for name, phone in contacts.items()])
+        all_contacts = "\n".join([f"{contact_name.title()}: {phone}" for contact_name, phone in contacts.items()])
         return all_contacts
     else:
         return "No contacts found."
-    
+
+def hello_command():
+    return "How can I help you?"
+
+def goodbye_command():
+    return "Goodbye!"
+
+def komand(command_parts):
+    command_key = command_parts[0]
+    command_arguments = command_parts[1:]
+
+    if command_key == "show" and command_arguments == ["all"]:
+        return show_all_contacts()
+    elif command_key == "good" and command_arguments == ["bye"]:
+        return goodbye_command()
+    elif command_key in command_handlers:
+        handler = command_handlers[command_key]
+        return handler(*command_arguments)
+    else:
+        return "Invalid command. Please try again."
+
+command_handlers = {
+    "hello": hello_command,
+    "exit": goodbye_command,
+    "close": goodbye_command,
+    "add": add_contact,
+    "change": change_phone,
+    "phone": get_phone
+}
 
 def main():
     while True:
-        command = input("Enter a command: ").lower().split()
-        if command[0] == "hello":
-            print("How can I help you?")
-        elif command[0] == "add":
-            if len(command) >= 3:
-                name = " ".join(command[1:-1])
-                phone = command[-1]
-                print(add_contact(name, phone))
-            else:
-                print("Invalid input. Please provide both name and phone number.")
-        elif command[0] == "change":
-            if len(command) >= 3:
-                name = " ".join(command[1:-1])
-                phone = command[-1]
-                print(change_phone(name, phone))
-            else:
-                print("Invalid input. Please provide both name and phone number.")
-        elif command[0] == "phone":
-            if len(command) >= 2:
-                name = " ".join(command[1:])
-                print(get_phone(name))
-            else:
-                print("Invalid input. Please provide a name.")
-        elif command[0] == "show":
-            if len(command) >= 2 and command[1] == "all":
-                print(show_all_contacts())
-            else:
-                print("Invalid input. Please provide a valid command.")
-        elif command[0] in ["good", "bye", "close", "exit"]:
-            print("Good bye!")
+        command = input("Enter a command: ").lower()
+        command_parts = command.split()
+        
+        result = komand(command_parts)
+        print(result)
+
+        if result == "Goodbye!":
             break
-        else:
-            print("Invalid command. Please try again.")    
 
 if __name__ == "__main__":
     main()
